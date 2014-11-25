2675292689c7d5035399fa4957925a6cffda467b
wooorm <tituswormer@gmail.com>
2014-11-25 17:15:31 +0100

    Refactor `script/build-data.js`

--- script/build-data.js
+++ script/build-data.js
@@ -1,16 +1,35 @@
 'use strict';
 
-var fs, textToJSON, data;
+/**
+ * Dependencies.
+ */
 
-textToJSON = require('plain-text-data-to-json');
+var fs,
+    toJSON;
+
+toJSON = require('plain-text-data-to-json');
 fs = require('fs');
 
-data = textToJSON(fs.readFileSync('data/AFINN/AFINN-111.txt', 'utf8'), {
+/**
+ * Data.
+ */
+
+var data;
+
+data = toJSON(fs.readFileSync('data/AFINN/AFINN-111.txt', 'utf8'), {
     'delimiter': '\t'
 });
+
+/**
+ * Parse.
+ */
 
 Object.keys(data).forEach(function (key) {
     data[key] = Number(data[key]);
 });
+
+/**
+ * Write.
+ */
 
 fs.writeFileSync('data/afinn-111.json', JSON.stringify(data));

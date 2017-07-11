947c893febc3c42022aa2ea51cdd4b3c7a8feb47
Titus Wormer <tituswormer@gmail.com>
2017-07-11 21:56:28 +0200

    Refactor code-style

--- build.js
+++ build.js
@@ -1,6 +1,5 @@
 'use strict';
 
-/* Dependencies. */
 var fs = require('fs');
 var http = require('http');
 var Transform = require('readable-stream').Transform;
@@ -10,34 +9,34 @@ var wrap = require('wrap-stream');
 var join = require('join-stream');
 var bail = require('bail');
 
-/* Constants. */
-var url = 'http://www2.imm.dtu.dk/pubdb/views/edoc_download.php/6010/zip/imm6010.zip';
+http.get('http://www2.imm.dtu.dk/pubdb/views/edoc_download.php/6010/zip/imm6010.zip', onresponse);
 
-/* Read. */
-http
-  .get(url, function (res) {
-    res
-      .resume()
-      .pipe(new unzip.Parse())
-      .on('error', bail)
-      .on('entry', function (entry) {
-        if (entry.path !== 'AFINN/AFINN-111.txt') {
-          entry.autodrain();
-          return;
-        }
+function onresponse(res) {
+  res
+    .resume()
+    .pipe(new unzip.Parse())
+    .on('error', bail)
+    .on('entry', onentry);
+}
 
-        entry
-          .pipe(csv({delimiter: '\t', objectMode: true}))
-          .pipe(new Transform({
-            objectMode: true,
-            transform: function (chunk, encoding, callback) {
-              this.push('  "' + chunk[0] + '": ' + chunk[1]);
-              callback();
-            }
-          }))
-          .pipe(join(',\n'))
-          .pipe(wrap('{\n', '\n}\n'))
-          .pipe(fs.createWriteStream('index.json'));
-      });
-  })
-  .on('error', bail);
+function onentry(entry) {
+  if (entry.path !== 'AFINN/AFINN-111.txt') {
+    entry.autodrain();
+    return;
+  }
+
+  entry
+    .pipe(csv({delimiter: '\t', objectMode: true}))
+    .pipe(new Transform({
+      objectMode: true,
+      transform: transform
+    }))
+    .pipe(join(',\n'))
+    .pipe(wrap('{\n', '\n}\n'))
+    .pipe(fs.createWriteStream('index.json'));
+
+  function transform(chunk, encoding, callback) {
+    this.push('  "' + chunk[0] + '": ' + chunk[1]);
+    callback();
+  }
+}
--- test.js
+++ test.js
@@ -1,10 +1,8 @@
 'use strict';
 
-/* Dependencies. */
 var test = require('tape');
 var afinn = require('./');
 
-/* Tests. */
 test('afinn', function (t) {
   t.equal(afinn.positive, 2);
   t.equal(afinn['self-deluded'], -2);

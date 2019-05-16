f568277c88c8f6350489228568d6f8801e5c61ed
Titus Wormer <tituswormer@gmail.com>
2019-05-16 19:53:27 +0200

    Use `prettier`

--- build.js
+++ build.js
@@ -1,42 +1,47 @@
-'use strict';
+'use strict'
 
-var fs = require('fs');
-var https = require('https');
-var Transform = require('readable-stream').Transform;
-var unzip = require('unzip');
-var csv = require('csv-streamify');
-var wrap = require('wrap-stream');
-var join = require('join-stream');
-var bail = require('bail');
+var fs = require('fs')
+var https = require('https')
+var Transform = require('readable-stream').Transform
+var unzip = require('unzip')
+var csv = require('csv-streamify')
+var wrap = require('wrap-stream')
+var join = require('join-stream')
+var bail = require('bail')
 
-https.get('https://www2.imm.dtu.dk/pubdb/views/edoc_download.php/6010/zip/imm6010.zip', onresponse);
+https.get(
+  'https://www2.imm.dtu.dk/pubdb/views/edoc_download.php/6010/zip/imm6010.zip',
+  onresponse
+)
 
 function onresponse(res) {
   res
     .resume()
     .pipe(new unzip.Parse())
     .on('error', bail)
-    .on('entry', onentry);
+    .on('entry', onentry)
 }
 
 function onentry(entry) {
   if (entry.path !== 'AFINN/AFINN-111.txt') {
-    entry.autodrain();
-    return;
+    entry.autodrain()
+    return
   }
 
   entry
     .pipe(csv({delimiter: '\t', objectMode: true}))
-    .pipe(new Transform({
-      objectMode: true,
-      transform: transform
-    }))
+    .pipe(
+      new Transform({
+        objectMode: true,
+        transform: transform
+      })
+    )
     .pipe(join(',\n'))
     .pipe(wrap('{\n', '\n}\n'))
-    .pipe(fs.createWriteStream('index.json'));
+    .pipe(fs.createWriteStream('index.json'))
 
   function transform(chunk, encoding, callback) {
-    this.push('  "' + chunk[0] + '": ' + chunk[1]);
-    callback();
+    this.push('  "' + chunk[0] + '": ' + chunk[1])
+    callback()
   }
 }
--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-'use strict';
+'use strict'
 
-var test = require('tape');
-var afinn = require('.');
+var test = require('tape')
+var afinn = require('.')
 
-test('afinn', function (t) {
-  t.equal(afinn.positive, 2);
-  t.equal(afinn['self-deluded'], -2);
+test('afinn', function(t) {
+  t.equal(afinn.positive, 2)
+  t.equal(afinn['self-deluded'], -2)
 
-  t.end();
-});
+  t.end()
+})

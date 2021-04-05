65bf982c40a87e4bebc0423db5fcfdeee8b1401c
Titus Wormer <tituswormer@gmail.com>
2021-04-05 14:30:27 +0200

    Update build script

--- build.js
+++ build.js
@@ -3,15 +3,12 @@
 var fs = require('fs')
 var path = require('path')
 var https = require('https')
-var Transform = require('readable-stream').Transform
 var yauzl = require('yauzl')
-var csv = require('csv-streamify')
-var wrap = require('wrap-stream')
-var join = require('join-stream')
+var dsv = require('d3-dsv')
+var concat = require('concat-stream')
 var bail = require('bail')
 
-var endpoint =
-  'https://www2.imm.dtu.dk/pubdb/views/edoc_download.php/6010/zip/imm6010.zip'
+var endpoint = 'https://www2.imm.dtu.dk/pubdb/edoc/imm6010.zip'
 
 var found = false
 
@@ -47,14 +44,20 @@ function onopen(error, archive) {
 
   function onreadstream(error, rs) {
     bail(error)
-
-    rs.pipe(csv({delimiter: '\t', objectMode: true}))
-      .pipe(new Transform({objectMode: true, transform: transform}))
-      .pipe(join(',\n'))
-      .pipe(wrap('{\n', '\n}\n'))
-      .pipe(fs.createWriteStream('index.json'))
-
+    rs.pipe(concat(onconcat))
     rs.on('end', read)
+  }
+
+  function onconcat(x) {
+    var data = {}
+    var rows = dsv.tsvParse('key\tvalue\n' + String(x))
+    var index = -1
+
+    while (++index < rows.length) {
+      data[rows[index].key] = Number.parseInt(rows[index].value, 10)
+    }
+
+    fs.writeFile('index.json', JSON.stringify(data, null, 2) + '\n', bail)
   }
 
   function read() {
@@ -66,9 +69,4 @@ function onend() {
   if (!found) {
     throw new Error('File not found')
   }
-}
-
-function transform(chunk, encoding, callback) {
-  this.push('  "' + chunk[0] + '": ' + chunk[1])
-  callback()
 }
